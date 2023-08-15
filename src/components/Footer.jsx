3528eba6001103cc01c69e6fd91d5f880e428e7f
Atharva <atharvad931@gmail.com>
2023-08-16 00:28:27 +0530

    Added navbar and footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React, {Fragment, useEffect, useState} from "react";
+import '../styles/footer.css';
+
+const Footer = () => {
+    return (
+        <footer className="footer">
+            <div>NTH2023</div>
+        </footer>
+    )
+}
+
+export default Footer;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import React, {Fragment, useEffect, useState} from "react";
+import '../styles/navbar.css';
+
+const Navbar = () => {
+    return (
+        <nav className="navbar">
+            <div clasName="navbar">I am a Navbar </div>
+        </nav>
+    )
+}
+
+export default Navbar;
