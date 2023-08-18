1d0342f7ee0aab1cfad928116b1bef048a891e1e
Karan-mundhada <92524719+Karan-mundhada@users.noreply.github.com>
2023-08-18 23:37:35 +0530

    navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import BackGround from './components/background'
-import Navbar from './components/Navbar'
-import Footer from './components/Footer'
+import { BrowserRouter, Route } from "react-router-dom";
+import "./App.css";
+import BackGround from "./components/background";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-    <Navbar/>
-    <BackGround/>
-    <Footer/>
+      <BrowserRouter>
+        <Navbar />
+        {/* <Route path="/" exact component={Home} />
+        <Route path="/about" component={About} />
+        <Route path="/leaderboard" component={Leaderboard} />
+        <Route path="/contact" component={Contact} />
+        <Route path="/login" component={Login} /> */}
+      </BrowserRouter>
+      <BackGround />
+      <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,20 @@
-import React, { Fragment, useEffect, useState } from "react";
+import React from "react";
+import { Link } from "react-router-dom";
 import "../styles/navbar.css";
 
 const Navbar = () => {
   return (
-    <nav className="navbar">
-      <div className="navbar">I am a not Navbar </div>
-    </nav>
+    <div className="navbar">
+      <Link to="https://ctd.pictieee.in/">
+        <img src="NTH-Comic\src\assets\bitly.png"></img>
+      </Link>
+      <Link to="/">Home</Link>
+      <Link to="/about">About</Link>
+      <Link to="/leaderboard">Leaderboard</Link>
+      <Link to="/contact">Contact</Link>
+      <Link to="/login">Login</Link>
+      <Link to="https://www.ieee.org/">IEEE logo</Link>
+    </div>
   );
 };
 
