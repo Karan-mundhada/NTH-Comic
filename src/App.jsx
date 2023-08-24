d00a1cb8bc5c71e9975ca9e616a9db65cb6da0a4
Riddhi Kulkarni <riddhikul26@gmail.com>
2023-08-24 17:43:44 +0530

    Added bg to all pages

--- src/App.jsx
+++ src/App.jsx
@@ -5,45 +5,26 @@ import "./App.css";
 import BackGround from "./components/background";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
-import Pane from "./components/Pane"
-import Home from "./components/Home"; // Import your Home component
+import Pane from "./components/Pane";
 import About from "./components/About"; // Import your About component
 
-// const routes = [{
-//   path: '/',
-//   component: <App/>,
-  
-// },{
-//   path:'/home',
-//   component:<Home/>
-// },{
-//   path:'/about',
-//   component:<About/>
-// }]
-
 const App = () => {
-  
-
   return (
-    
     <BrowserRouter>
-    <Navbar/>
-    
-    <Routes>
-      
-      <Route path="/about" element={<About/>}>
-      
-      </Route>
-      {/* <Route index element={<Home />} />
+      <Navbar />
+      <BackGround />
+      <Routes>
+        <Route path="/" element={<Pane />}></Route>
+        <Route path="/about" element={<About />}></Route>
+        {/* <Route index element={<Home />} />
       <Route path="about" element={<About />} />
       Add other Route components for Leaderboard, Contact, and Login */}
-    </Routes>
-    <BackGround /> 
-    <Pane/>
-     <Footer />
-  </BrowserRouter>
-    
+      
+      </Routes>
+      {/*  */}
+      <Footer />
+    </BrowserRouter>
   );
-}
+};
 
 export default App;
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,20 +1,15 @@
-// eslint-disable-next-line no-unused-vars
-import React from "react"
-import Pane from '../components/Pane'
-import '../styles/pane.css'
+import React, {Fragment, useEffect, useState} from "react";
+import '../styles/pane.css';
+
 const Home = () => {
-  return (
-    <>
-    <div><div className="pane">
-            <div>
+    return (
+        <div>
             <img className="nth-logo" src="../public/static/nth-logo.png"/>
             <h1 className="nth-text">Network Treasure Hunt</h1>
             <h3 className="nth-text">Decrypt the Encrypted</h3>
             <p className="nth-text">The hunt begins in 10:10:10</p>
-            </div>
-        </div></div>
-    </>
-  )
+        </div>
+    )
 }
 
-export default Home
+export default Home;
